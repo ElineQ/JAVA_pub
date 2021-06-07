3f7146adef6f22036ae06c681a20e5e3e1e6243a
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-07 21:43:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let Name = 'Eline'; //string literal
+console.log("Hallo Winc Academy studenten");
+
+document.write(1230941 x 1823794)
+
+document.write(637263 / 54)
+
+
+let interestRate = 0,3;
+interestRate = 1;
+console.log(interestRate);
+
+let age = 25; //numer literal
+
+let isApproved = false; //Boolean literal (1 van de 2)
+
+let firstName = undefined;
+
+let selectedColor = null;
+
+
+var age = 19;
+if (age = > 18) {
+
+    document.write = "Come in!";
+
+} else {
+    document.write = Do not enter!;
+}
+
+var sekse = isFemale(Y);
+if (sekse = Y) {
+
+    document.write = "Welcome lady!"
+
+} else {
+    document.write = "Welcome sur!"
+
+
+
+    var driverStatus = < 2;
+
+    if (driverStatus = 1) {
+
+        document.write = "You are the BOB!"
+
+    } else {
+
+        document.write = "You can better get a cab!"
+    }
+
